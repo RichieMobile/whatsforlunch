7dcb50b38019ba8237f242853f89c4aa03f48b04
RichieMobile <ricky@Williams-MacBook-Pro.local>
2018-04-27 17:38:01 -0400

    Added random lunch selector.

--- lib/whatsforlunch_web/controllers/page_controller.ex
+++ lib/whatsforlunch_web/controllers/page_controller.ex
@@ -1,7 +1,17 @@
 defmodule WhatsforlunchWeb.PageController do
   use WhatsforlunchWeb, :controller
+  alias Whatsforlunch.Lunch
+  require Logger
 
   def index(conn, _params) do
     render conn, "index.html"
+  end
+
+  def random(conn, _params) do
+    Logger.info("Getting random restaurant")
+    restaurants = Lunch.list_restaurants
+    number = :rand.uniform(length(restaurants)) - 1 
+    restaurant = Enum.at(restaurants, number)
+    render(conn, "random.html", restaurant: restaurant)
   end
 end
