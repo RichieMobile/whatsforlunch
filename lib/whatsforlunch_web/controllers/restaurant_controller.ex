b4be2c44c3c18eb4a69995a731e2bfa33f3b5224
RichieMobile <ricky@Williams-MacBook-Pro.local>
2018-04-30 22:20:21 -0400

    Started adding an endpoint for searching yelp.

--- lib/whatsforlunch_web/controllers/restaurant_controller.ex
+++ lib/whatsforlunch_web/controllers/restaurant_controller.ex
@@ -3,6 +3,9 @@ defmodule WhatsforlunchWeb.RestaurantController do
 
   alias Whatsforlunch.Lunch
   alias Whatsforlunch.Lunch.Restaurant
+  alias Whatsforlunch.Yelp.YelpApi
+
+  require Logger
 
   def index(conn, _params) do
     restaurants = Lunch.list_restaurants()
@@ -23,6 +26,28 @@ defmodule WhatsforlunchWeb.RestaurantController do
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
+  end
+
+  def yelp_search(conn, %{"search" => search_params}) do
+    case YelpApi.search(search_params) do
+      {:ok, search_result} ->
+        search_result.businesses
+        |> map_search_result_to_restaurant()
+        |> Enum.each(&(Lunch.create_restaurant(&1)))
+      {:error, message} ->
+        Logger.info("Error searching yelp: #{message}")
+    end
+  end
+
+  defp map_search_result_to_restaurant([]), do: []
+  defp map_search_result_to_restaurant(business) do
+    Enum.map(business, fn(x) -> 
+      %Restaurant{
+        name: x.name,
+        website: x.url,
+        location: Enum.at(x.location.display_address, 0)
+      }
+    end)
   end
 
   def show(conn, %{"id" => id}) do
