16440e195fa30336a92a5152b484c7a3056b7768
RichieMobile <ricky@Williams-MacBook-Pro.local>
2018-07-04 14:24:42 -0400

    Initial implementation for presenting a range of restaurants.

--- lib/whatsforlunch_web/controllers/page_controller.ex
+++ lib/whatsforlunch_web/controllers/page_controller.ex
@@ -7,6 +7,34 @@ defmodule WhatsforlunchWeb.PageController do
     render conn, "index.html"
   end
 
+  def random_selection(conn, _params) do
+    render(conn, "random_selection.html")
+  end
+
+  def random_range(conn, params) do
+    Logger.info("Getting random range of restaurants: #{params["count"]}")
+
+    Lunch.list_restaurants
+    |> get_random_restaurants(params["count"])
+    |> render_restuarants(conn, "random_range.html")
+  end
+
+  defp get_random_restaurants([], _range) do
+    Logger.info("No restaurants configured, displaying error.")
+    %Lunch.Restaurant{name: "Oops!", location: "No restaurants configured"}
+  end
+
+  defp get_random_restaurants(restaurants, range) do
+    {rangeInt, _ok} = Integer.parse(range)
+    rests = Enum.map(1..rangeInt, fn(_a) -> get_random_restaurant(restaurants) end)
+    IO.inspect(rests)
+    rests
+  end
+
+  defp render_restuarants(restaurants, conn, template) do
+    render(conn, template, restaurants: restaurants)
+  end
+
   def random(conn, _params) do
     Logger.info("Getting random restaurant")
 
--- lib/whatsforlunch_web/router.ex
+++ lib/whatsforlunch_web/router.ex
@@ -17,9 +17,13 @@ defmodule WhatsforlunchWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
     get "/random", PageController, :random
+    get "/random_selection", PageController, :random_selection
+    post "/random_range", PageController, :random_range
 
     resources "/restaurants", RestaurantController
+
     get "/yelp_search", YelpController, :yelp_search
     post "/yelp_add", YelpController, :yelp_add
   end
